64c2f2ac4ace30cdc91d5bf1617848e2beab62a6
harvestcore <aagomezies@gmail.com>
2020-05-06 18:33:02 +0200

    1st commit

---
+++ app/src/main/java/com/agm/ipmanager/machines/MachinesFragment.java
@@ -0,0 +1,32 @@
+package com.agm.ipmanager.machines;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.agm.ipmanager.R;
+
+public class MachinesFragment extends Fragment {
+    public MachinesFragment() {
+    }
+
+    public static MachinesFragment newInstance(String param1, String param2) {
+        MachinesFragment fragment = new MachinesFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_machines, container, false);
+    }
+}
---
+++ app/src/main/java/com/agm/ipmanager/status/StatusFragment.java
@@ -0,0 +1,34 @@
+package com.agm.ipmanager.status;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.agm.ipmanager.R;
+
+public class StatusFragment extends Fragment {
+
+    public StatusFragment() {
+    }
+
+
+    public static StatusFragment newInstance() {
+        StatusFragment fragment = new StatusFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_status, container, false);
+    }
+}
